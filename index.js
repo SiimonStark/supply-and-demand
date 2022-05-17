badf51bfafc95f633b1459bcd92e19ebd37bf24a
SiimonStark <siimonstark@gmail.com>
2022-05-17 16:07:38 -0700

    Implented expectedDeliveryDate, Refactored state, WIP: finish refactor and debug of 'handleQueue'

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ class Store {
         this.sales = salesOrders;
         this.purchases = purchaseOrders;
         this.queue = [];
-        this.demand = 0;
-        this.supply = 0;
+        this.totalDemand = this.setTotals('sales');
+        this.totalSupply = this.setTotals('purchases');
+        this.demand = salesOrders[0].quantity || 0;
+        this.supply = purchaseOrders[0].quantity || 0;
         this.head = 0;
         this.tail = 0;
     }
@@ -16,49 +18,64 @@ class Store {
         this.fillQueue();
     }
     fillQueue() {
-        this.setTotals();
         this.sales.forEach((sale, index) => {
             console.log({ sale, index })
             if (sale.quantity < this.supply) {
-                this.head = index;
-                let queueObject = {
-                    saleID: sale.id,
-                    expectedDelivery: this.getDeliveryEstimate(sale.quantity)
-                }
-
+                this.handleQueue(sale);
             }
         });
+        console.log(this.showQueue());
     }
     showQueue() {
         console.log('Queue at this time: ')
         console.log(this.queue);
     }
-    getDeliveryEstimate(saleQuantity) {
-        let demand = saleQuantity;
+    handleQueue(sale, currentSupply) {
         let currentPurchaseOrder = this.purchases[0];
         if (!currentPurchaseOrder) {
             console.log('Message: Demand exceeds supply');
             return;
         }
 
-        console.log('new loop =========')
-        console.log({ currentPurchaseOrder })
-        console.log('supply ', currentPurchaseOrder.quantity, ' <= demand ', demand)
+        // Todo: Refactor using 'this' values
+        console.log('Annoying ## ', { supply: this.supply }) //REMOVE
+        let demand = sale.quantity;
+        let supply;
+        if (currentSupply) {
+            supply = currentPurchaseOrder.quantity + currentSupply;
+        } else {
+            supply = currentPurchaseOrder.quantity;
+        }
 
-        if (currentPurchaseOrder.quantity <= demand) {
+        console.log('new loop =========') //REMOVE
+        console.log({ currentPurchaseOrder }) //REMOVE
+        console.log('supply ', supply, ' <= demand ', demand) //REMOVE
+
+        if (supply <= demand) {
             this.purchases = this.purchases.slice(1);
-            demand = demand - currentPurchaseOrder.quantity;
-            console.log('after compute = ', { demand })
+            demand = demand - supply;
+            console.log('after compute = ', { demand, supply }) //REMOVE
             if (demand > 0) {
-                console.log('Need to run again ➿')
-                this.getDeliveryEstimate(demand);
+                console.log('Need to run again ♾') //REMOVE
+                this.handleQueue(sale, supply);
                 return;
             }
         } else {
             this.purchases[0].quantity = currentPurchaseOrder.quantity - demand;
-            console.log('before exiting loop ', { demand: 0, supply: this.purchases[0].quantity });
-            console.log('exit loop')
-            return currentPurchaseOrder.receiving;
+
+            let deliveryDate = new Date(this.purchases[0].receiving);
+            let shippingEstimate = 5;
+            let expectedShipping = (deliveryDate.getDate() + 1) + shippingEstimate;
+            deliveryDate.setDate(expectedShipping);
+            const formattedDeliveryDate = deliveryDate.toLocaleString();
+
+            let queueObject = {
+                saleID: sale.id, deliveryDate: formattedDeliveryDate
+            }
+            this.queue = [...this.queue, queueObject];
+            console.log('before exiting loop ', { queueObject, demand: 0, supply: this.purchases[0].quantity }); //REMOVE
+            console.log('exit loop') //REMOVE
+            return;
         }
 
     }
@@ -91,21 +108,22 @@ class Store {
         );
     }
     evaluateInventory() {
-        this.setTotals();
-
-        if (this.demand > this.supply) {
-            console.log(`Message: A new purchase order for ${this.demand - this.supply} items needs to be made`);
+        if (this.totalDemand > this.totalSupply) {
+            console.log(`Message: A new purchase order for ${this.totalDemand - this.totalSupply} items needs to be made`);
         } else {
-            console.log(`Message: Supply is ahead by ${this.supply - this.demand}`);
+            console.log(`Message: Supply is ahead by ${this.totalSupply - this.totalDemand}`);
         }
     }
-    setTotals() {
-        this.demand = this.sales.reduce((total, order) => {
+    setTotals(type) {
+        return this[type].reduce((total, order) => {
             return total += order.quantity;
         }, 0);
-        this.supply = this.purchases.reduce((total, order) => {
-            return total += order.quantity;
-        }, 0);
+        // this.demand = this.sales.reduce((total, order) => { //REMOVE
+        //     return total += order.quantity; //REMOVE
+        // }, 0); //REMOVE
+        // this.supply = this.purchases.reduce((total, order) => { //REMOVE
+        //     return total += order.quantity; //REMOVE
+        // }, 0); //REMOVE
     }
 }
 
