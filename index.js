ea93cfe97cad23e83df852104f5773cb6df965f8
siimonstark <siimonstark@gmail.com>
2022-05-17 02:44:48 -0700

    WIP: fillQueue. Currently: traversing both supply and demand reach end of supply successfully

--- index.js
+++ index.js
@@ -5,8 +5,62 @@ class Store {
         this.sales = salesOrders;
         this.purchases = purchaseOrders;
         this.queue = [];
+        this.demand = 0;
+        this.supply = 0;
         this.head = 0;
         this.tail = 0;
+    }
+    initiateStore() {
+        this.sortOrdersByDate('sales');
+        this.sortOrdersByDate('purchases');
+        this.fillQueue();
+    }
+    fillQueue() {
+        this.setTotals();
+        this.sales.forEach((sale, index) => {
+            console.log({ sale, index })
+            if (sale.quantity < this.supply) {
+                this.head = index;
+                let queueObject = {
+                    saleID: sale.id,
+                    expectedDelivery: this.getDeliveryEstimate(sale.quantity)
+                }
+
+            }
+        });
+    }
+    showQueue() {
+        console.log('Queue at this time: ')
+        console.log(this.queue);
+    }
+    getDeliveryEstimate(saleQuantity) {
+        let demand = saleQuantity;
+        let currentPurchaseOrder = this.purchases[0];
+        if (!currentPurchaseOrder) {
+            console.log('Message: Demand exceeds supply');
+            return;
+        }
+
+        console.log('new loop =========')
+        console.log({ currentPurchaseOrder })
+        console.log('supply ', currentPurchaseOrder.quantity, ' <= demand ', demand)
+
+        if (currentPurchaseOrder.quantity <= demand) {
+            this.purchases = this.purchases.slice(1);
+            demand = demand - currentPurchaseOrder.quantity;
+            console.log('after compute = ', { demand })
+            if (demand > 0) {
+                console.log('Need to run again ➿')
+                this.getDeliveryEstimate(demand);
+                return;
+            }
+        } else {
+            this.purchases[0].quantity = currentPurchaseOrder.quantity - demand;
+            console.log('before exiting loop ', { demand: 0, supply: this.purchases[0].quantity });
+            console.log('exit loop')
+            return currentPurchaseOrder.receiving;
+        }
+
     }
     updateOrder(id, newQuantity) {
         let typeOfOrder = id.split('')[0] === 's' ? 'sales' : 'purchases';
@@ -36,6 +90,23 @@ class Store {
         }
         );
     }
+    evaluateInventory() {
+        this.setTotals();
+
+        if (this.demand > this.supply) {
+            console.log(`Message: A new purchase order for ${this.demand - this.supply} items needs to be made`);
+        } else {
+            console.log(`Message: Supply is ahead by ${this.supply - this.demand}`);
+        }
+    }
+    setTotals() {
+        this.demand = this.sales.reduce((total, order) => {
+            return total += order.quantity;
+        }, 0);
+        this.supply = this.purchases.reduce((total, order) => {
+            return total += order.quantity;
+        }, 0);
+    }
 }
 
 let currentStore;
@@ -47,9 +118,10 @@ function allocate(salesOrders, purchaseOrders) {
 
     if (!currentStore) {
         currentStore = new Store(salesOrders, purchaseOrders);
-        currentStore.sortOrdersByDate('sales');
-        currentStore.sortOrdersByDate('purchases');
-        console.log('After Sort => ', currentStore) //REMOVE
+        currentStore.initiateStore();
+
+        // currentStore.evaluateInventory(); //REMOVE
+        // console.log('After Sort => ', currentStore) //REMOVE
         return;
     } else {
         console.log('Store exists, updating current store');
